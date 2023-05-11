07bb5a081bd4e5811075728af7993dba980a6a93
Igor <115535399+IgorVinnik85@users.noreply.github.com>
2023-05-11 21:48:14 +0300

    console

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,6 @@ export class App extends Component {
   btnTitle = Object.keys(this.state);
 
   addAllFeedback = event => {
-    console.log(event.currentTarget.textContent);
     const btnName = event.currentTarget.textContent.toLowerCase();
 
     this.setState(prevState => {
